bbff25c8eadbea6d7f7ea8031ba080f13a96ab87
Sean Huang <seanhuang2005@gmail.com>
2026-09-10 18:10:22 -0600

    during lab - mostly complete

--- app/src/main/java/com/example/listycity/MainActivity.kt
+++ app/src/main/java/com/example/listycity/MainActivity.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutBounds
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
+import androidx.compose.foundation.lazy.items
 
 class CityRepository {
     private val _cities = mutableStateListOf(
@@ -40,6 +55,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
                         cities = cityRepository.cities,
+                        onAddCity = {cityRepository.addCity(it)},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -49,16 +65,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun CityRow(city: String) {
+    Text(
+        text = city,
+        fontSize = 28.sp,
+        modifier = Modifier.fillMaxWidth().padding(horizontal=19.dp, vertical=14.dp)
+    )
+}
+
+@Composable
 fun CityListScreen(
     cities: List<String>,
+    onAddCity: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var newCityName by remember {mutableStateOf("")}
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = {newCityName = it},
+                label = {Text("City name")},
+                modifier = Modifier.weight(1f)
+            )
+
+            Spacer(modifier = modifier.width(8.dp))
+
+            Button(
+                onClick = {
+                    if (newCityName.isNotBlank()) {
+                        onAddCity(newCityName)
+                        newCityName = ""
+                    }
+                }
+            ) {
+                Text("Add City")
+            }
+        }
+    }
+
     LazyColumn(modifier = modifier.fillMaxSize()) {
         items(cities) { city ->
             CityRow(city = city)
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
